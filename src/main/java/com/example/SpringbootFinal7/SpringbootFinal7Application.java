240be3802ab0a075da0ecb327006796ac0e43746
dinagasa <divyanagasai2014@gmail.com>
2023-06-17 17:16:26 +0530

    First commit

---
+++ src/main/java/com/example/SpringbootFinal7/SpringbootFinal7Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootFinal7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFinal7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFinal7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringbootFinal7/SpringbootFinal7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootFinal7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFinal7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
